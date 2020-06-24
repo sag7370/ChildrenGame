8d24092f5aee0d0c25dadba7adfa3926119decd8
sag <sag7370@163.com>
2020-06-25 01:41:07 +0800

    第一次提交

---
+++ Assets/Scripts/MusicPause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MusicPause : MonoBehaviour {
+	// Use this for initialization
+	AudioSource[] As;
+	void Awake()
+    {
+		As = GetComponentsInChildren<AudioSource>();
+	}
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public  void musicPause()
+    {
+		foreach (AudioSource Ass in As)
+        {
+			if (Ass != null)
+            {
+				Ass.Stop();
+			}
+        }
+    }
+}
---
+++ Assets/Scripts/MusicTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicTest : MonoBehaviour {
+	AudioSource As;
+	Button btn;
+	GameObject father;
+	// Use this for initialization
+	void Start () {
+		As = GetComponent<AudioSource>();
+		btn = GetComponent<Button>();
+		btn.onClick.AddListener(OnBtnClick);//绑定按钮事件
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnBtnClick()
+	{
+		father = transform.parent.gameObject;
+		//获取父节点的MusicPause脚本
+		var fa = father.GetComponent<MusicPause>();
+		fa.musicPause();
+		As.Play();
+    }
+}
